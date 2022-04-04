e39b4d8d150373e3e7c6da14bf588d862a8e050d
Abdelbouakka <102311234+Abdelbouakka@users.noreply.github.com>
2022-04-04 16:17:20 +0200

    Update job.java

--- job.java
+++ job.java
@@ -1,4 +1,4 @@
-public class print {
+public class job {
 	
 	public static void main(String[] args) {
 		System.out.println("Soc Abdelhakim Bouakka Bouakka i estic fent l’examen de la UF2 del M01 de DAM.");
